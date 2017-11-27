2c037516516131385f8f00d7c7ab4c31288d3cf6
Christopher Dembia <cld72@cornell.edu>
2017-11-27 15:11:33 -0500

    Whole body tracking: increase segment masses
    Make it easy to choose how many segments to use; give option of adding in contact model.

--- Muscollo/Muscollo/MucoTropterSolver.cpp
+++ Muscollo/Muscollo/MucoTropterSolver.cpp
@@ -378,6 +378,7 @@ MucoSolution MucoTropterSolver::solveImpl() const {
     //for (int i = 0; i < getProperty_optim_solver_options(); ++i) {
     //    optsolver.set_advanced_option(TODO);
     //}
+    //optsolver.set_advanced_option_string("print_timing_statistics", "yes");
 
     tropter::OptimalControlIterate tropIterate = convert(getGuess());
     tropter::OptimalControlSolution tropSolution = dircol.solve(tropIterate);
