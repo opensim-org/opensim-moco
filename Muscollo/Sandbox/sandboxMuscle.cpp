cd58329298a44ce50f03d91c6b7d7f585f648977
Christopher Dembia <cld72@cornell.edu>
2018-03-14 11:58:37 -0700

    test2DOFs2Muscles, ignoring tendon compliance.

--- Muscollo/Sandbox/sandboxMuscle.cpp
+++ Muscollo/Sandbox/sandboxMuscle.cpp
@@ -449,11 +449,210 @@ void testHangingMuscleMinimumTime(bool ignoreTendonCompliance) {
     // lengths in Tropter!!!!!!!!!!!!!! (in explicit mode).
 }
 
+/// Move a point mass from a fixed starting state to a fixed end
+/// position and velocity, in fixed time, with minimum effort. The point mass
+/// has 2 DOFs (x and y translation).
+///
+///                            |< d >|< d >|
+///                    ----------------------
+///                             \         /
+///                              .       .
+///                   left muscle \     / right muscle
+///                                .   .
+///                                 \ /
+///                                  O mass
+///
+/// Here's a sketch of the problem we solve, with activation and fiber dynamics.
+/// @verbatim
+///   minimize   int_t (aL^2 + aR^2) dt
+///   subject to multibody dynamics
+///              muscle activation dynamics
+///              muscle fiber dynamics
+///              x(0) = -0.03
+///              y(0) = -d
+///              vx(0) = 0
+///              vy(0) = 0
+///              aL(0) = 0.01
+///              aR(0) = 0.01
+///              vmL(0) = 0
+///              vmR(0) = 0
+///              x(0.5) = +0.03
+///              y(0.5) = -d + 0.05
+///              vx(0.5) = 0
+///              vy(0.5) = 0
+/// @endverbatim
+Model create2DOFs2MusclesModel(const double& width, bool ignoreTendonCompliance)
+{
+
+    using SimTK::Vec3;
+
+    Model model;
+    model.setName("block2dof2musc");
+
+    // Massless intermediate body.
+    auto* intermed = new Body("intermed", 0, Vec3(0), SimTK::Inertia(0));
+    model.addComponent(intermed);
+    auto* body = new Body("body", 1, Vec3(0), SimTK::Inertia(1));
+    model.addComponent(body);
+
+    // TODO Muscollo does not support locked coordinates yet.
+    // Allow translation along x and y; disable rotation about z.
+    // auto* joint = new PlanarJoint();
+    // joint->setName("joint");
+    // joint->connectSocket_parent_frame(model.getGround());
+    // joint->connectSocket_child_frame(*body);
+    // auto& coordTX = joint->updCoordinate(PlanarJoint::Coord::TranslationX);
+    // coordTX.setName("tx");
+    // auto& coordTY = joint->updCoordinate(PlanarJoint::Coord::TranslationY);
+    // coordTY.setName("ty");
+    // auto& coordRZ = joint->updCoordinate(PlanarJoint::Coord::RotationZ);
+    // coordRZ.setName("rz");
+    // coordRZ.setDefaultLocked(true);
+    // model.addComponent(joint);
+    auto* jointX = new SliderJoint();
+    jointX->setName("jtx");
+    jointX->connectSocket_parent_frame(model.getGround());
+    jointX->connectSocket_child_frame(*intermed);
+    auto& coordX = jointX->updCoordinate(SliderJoint::Coord::TranslationX);
+    coordX.setName("tx");
+    model.addComponent(jointX);
+
+    // The joint's x axis must point in the global "+y" direction.
+    auto* jointY = new SliderJoint("jty",
+            *intermed, Vec3(0), Vec3(0, 0, 0.5 * SimTK::Pi),
+            *body, Vec3(0), Vec3(0, 0, .5 * SimTK::Pi));
+    auto& coordY = jointY->updCoordinate(SliderJoint::Coord::TranslationX);
+    coordY.setName("ty");
+    model.addComponent(jointY);
+
+    {
+        auto* actuL = new DeGrooteFregly2016Muscle();
+        actuL->setName("left");
+        actuL->set_ignore_tendon_compliance(ignoreTendonCompliance);
+        actuL->set_max_isometric_force(40);
+        actuL->set_optimal_fiber_length(.20);
+        actuL->set_tendon_slack_length(0.10);
+        actuL->set_pennation_angle_at_optimal(0.0);
+        actuL->addNewPathPoint("origin", model.updGround(),
+                Vec3(-width, 0, 0));
+        actuL->addNewPathPoint("insertion", *body, Vec3(0));
+        model.addForce(actuL);
+    }
+    {
+        auto* actuR = new DeGrooteFregly2016Muscle();
+        actuR->setName("right");
+        actuR->set_ignore_tendon_compliance(ignoreTendonCompliance);
+        actuR->set_max_isometric_force(40);
+        actuR->set_optimal_fiber_length(.21);
+        actuR->set_tendon_slack_length(0.09);
+        actuR->set_pennation_angle_at_optimal(0.0);
+        actuR->addNewPathPoint("origin", model.updGround(),
+                Vec3(+width, 0, 0));
+        actuR->addNewPathPoint("insertion", *body, Vec3(0));
+        model.addForce(actuR);
+    }
+
+    model.print("sandboxMuscle_2dof2musc.osim");
+    // model.setUseVisualizer(true);
+    // SimTK::State s = model.initSystem();
+    // model.equilibrateMuscles(s);
+    // model.updMatterSubsystem().setShowDefaultGeometry(true);
+    // model.updVisualizer().updSimbodyVisualizer().setBackgroundType(
+    //         SimTK::Visualizer::GroundAndSky);
+    // model.getVisualizer().show(s);
+    // std::cin.get();
+    // Manager manager(model, s);
+    // manager.integrate(1.0);
+
+    // TODO muscle is generating negative forces!
+
+    return model;
+}
+
+void test2DOFs2Muscles(bool ignoreTendonCompliance) {
+
+    const SimTK::Real width = 0.2;
+    const SimTK::Real initActivation = 0.01;
+    const SimTK::Real initTX = -0.03;
+    const SimTK::Real initTY = -width;
+
+    Model model = create2DOFs2MusclesModel(width, ignoreTendonCompliance);
+
+    SimTK::State state = model.initSystem();
+
+    model.setStateVariableValue(state, "jtx/tx/value", initTX);
+    model.setStateVariableValue(state, "jty/ty/value", initTY);
+    model.setStateVariableValue(state, "left/activation", initActivation);
+    model.setStateVariableValue(state, "right/activation", initActivation);
+    model.equilibrateMuscles(state);
+
+    // Manager manager(model, state);
+    // manager.getIntegrator().setAccuracy(1e-10);
+    // manager.getIntegrator().setMaximumStepSize(0.001);
+    // manager.integrate(2.0);
+    // // TODO STOFileAdapter::write(rep->getTable(), "DEBUG_sandboxMuscle_reporter.sto");
+    // visualize(model, manager.getStateStorage());
+
+    // Minimum effort trajectory optimization.
+    // ---------------------------------------
+    MucoSolution solutionTrajOpt;
+    {
+        MucoTool muco;
+        MucoProblem& problem = muco.updProblem();
+        problem.setModel(model);
+        problem.setTimeBounds(0, 0.5);
+        problem.setStateInfo("jtx/tx/value", {-0.03, 0.03}, initTX, 0.03);
+        problem.setStateInfo("jty/ty/value", {-2 * width, 0},
+                initTY, -width + 0.05);
+        problem.setStateInfo("jtx/tx/speed", {-15, 15}, 0, 0);
+        problem.setStateInfo("jty/ty/speed", {-15, 15}, 0, 0);
+        if (!ignoreTendonCompliance) {
+            problem.setStateInfo("left/norm_fiber_length", {0.6, 1.8},
+                    model.getStateVariableValue(state,
+                            "left/norm_fiber_length"));
+            problem.setStateInfo("right/norm_fiber_length", {0.6, 1.8},
+                    model.getStateVariableValue(state,
+                            "right/norm_fiber_length"));
+        }
+        // OpenSim might not allow activations of 0.
+        problem.setStateInfo("left/activation", {0.01, 1}, initActivation);
+        problem.setStateInfo("right/activation", {0.01, 1}, initActivation);
+        problem.setControlInfo("left", {0.01, 1});
+        problem.setControlInfo("right", {0.01, 1});
+
+        // TODO change to activation?
+        problem.addCost(MucoControlCost());
+
+        MucoTropterSolver& solver = muco.initSolver();
+        solver.set_optim_sparsity_detection("initial-guess");
+        MucoIterate guessForwardSim = solver.createGuess("time-stepping");
+        solver.setGuess(guessForwardSim);
+        guessForwardSim.write("sandboxMuscle_guess_forward_sim.sto");
+        // std::cout << "Guess from forward sim: "
+        //         << guessForwardSim.getStatesTrajectory() << std::endl;
+        muco.visualize(guessForwardSim);
+
+        solutionTrajOpt = muco.solve();
+        std::string solutionFilename = "sandboxMuscle_2dof2musc_solution.sto";
+        solutionTrajOpt.write(solutionFilename);
+        muco.visualize(solutionTrajOpt);
+
+        // TODO compare solution to pure tropter problem
+        // (test2Muscles2DOFsDeGrooteFregly2016.cpp).
+    }
+
+    // TODO look into negative muscle forces!
+
+}
+
 int main() {
     testDeGrooteFregly2016Muscle();
 
     testHangingMuscleMinimumTime(true);
     testHangingMuscleMinimumTime(false);
+
+    test2DOFs2Muscles(true);
+    // TODO test2DOFs2Muscles(false);
 
     return EXIT_SUCCESS;
 }
