10ce4f7cc2b56b882453f0789811dd006882bf7e
Christopher Dembia <cld72@cornell.edu>
2019-01-16 15:55:13 -0800

    Renaming to Moco.

--- Moco/Bindings/OpenSimHeaders_moco.h
+++ Moco/Bindings/OpenSimHeaders_moco.h
@@ -4,19 +4,19 @@
 */
 
 #include <Moco/osimMocoDLL.h>
-#include <Moco/MocoCost.h>
+#include <Moco/MocoCost/MocoCost.h>
 #include <Moco/MocoWeightSet.h>
-#include <Moco/MocoStateTrackingCost.h>
-#include <Moco/MocoMarkerTrackingCost.h>
-#include <Moco/MocoMarkerEndpointCost.h>
-#include <Moco/MocoControlCost.h>
-#include <Moco/MocoJointReactionNormCost.h>
+#include <Moco/MocoCost/MocoStateTrackingCost.h>
+#include <Moco/MocoCost/MocoMarkerTrackingCost.h>
+#include <Moco/MocoCost/MocoMarkerEndpointCost.h>
+#include <Moco/MocoCost/MocoControlCost.h>
+#include <Moco/MocoCost/MocoJointReactionNormCost.h>
 #include <Moco/MocoBounds.h>
 #include <Moco/MocoProblem.h>
 #include <Moco/MocoParameter.h>
 #include <Moco/MocoIterate.h>
 #include <Moco/MocoTropterSolver.h>
-#include <Moco/MucoCasADiSolver/MucoCasADiSolver.h>
+#include <Moco/MocoCasADiSolver/MocoCasADiSolver.h>
 #include <Moco/MocoTool.h>
 #include <Moco/ActivationCoordinateActuator.h>
 #include <Moco/MocoUtilities.h>
