fb42f39077c1834e2315c1afe8aedb830ba0646f
Antoine Falisse <antoinefalisse@gmail.com>
2019-11-29 11:49:34 +0100

    first pass to define muscle model

--- Moco/Moco/Components/SmoothBhargava2004Metabolics.cpp
+++ Moco/Moco/Components/SmoothBhargava2004Metabolics.cpp
@@ -26,16 +26,16 @@
 using namespace OpenSim;
 
 //=============================================================================
-//  SmoothBhargava2004Metabolics_MetabolicMusclePrameters
+//  SmoothBhargava2004Metabolics_MetabolicMuscleParameters
 //=============================================================================
 
-SmoothBhargava2004Metabolics_MetabolicMusclePrameters::
-SmoothBhargava2004Metabolics_MetabolicMusclePrameters() {
+SmoothBhargava2004Metabolics_MetabolicMuscleParameters::
+SmoothBhargava2004Metabolics_MetabolicMuscleParameters() {
     constructProperties();
 }
 
-SmoothBhargava2004Metabolics_MetabolicMusclePrameters::
-SmoothBhargava2004Metabolics_MetabolicMusclePrameters(
+SmoothBhargava2004Metabolics_MetabolicMuscleParameters::
+SmoothBhargava2004Metabolics_MetabolicMuscleParameters(
         const std::string& muscleName, double ratio_slow_twitch_fibers,
         double muscle_mass) {
     constructProperties();
@@ -51,8 +51,8 @@ SmoothBhargava2004Metabolics_MetabolicMusclePrameters(
     }
 }
 
-SmoothBhargava2004Metabolics_MetabolicMusclePrameters::
-SmoothBhargava2004Metabolics_MetabolicMusclePrameters(
+SmoothBhargava2004Metabolics_MetabolicMuscleParameters::
+SmoothBhargava2004Metabolics_MetabolicMuscleParameters(
         const std::string& muscleName,
         double ratio_slow_twitch_fibers,
         double activation_constant_slow_twitch,
@@ -77,7 +77,7 @@ SmoothBhargava2004Metabolics_MetabolicMusclePrameters(
     }
 }
 
-void SmoothBhargava2004Metabolics_MetabolicMusclePrameters::
+void SmoothBhargava2004Metabolics_MetabolicMuscleParameters::
 setMuscleMass()
 {
     if (get_use_provided_muscle_mass())
@@ -89,7 +89,7 @@ setMuscleMass()
         }
 }
 
-void SmoothBhargava2004Metabolics_MetabolicMusclePrameters::
+void SmoothBhargava2004Metabolics_MetabolicMuscleParameters::
 constructProperties()
 {
     // Specific tension of mammalian muscle (Pascals (N/m^2)).
@@ -110,10 +110,70 @@ constructProperties()
 //=============================================================================
 //  SmoothBhargava2004Metabolics
 //=============================================================================
+SmoothBhargava2004Metabolics::SmoothBhargava2004Metabolics()
+{
+    constructProperties();
+}
+
+SmoothBhargava2004Metabolics::SmoothBhargava2004Metabolics(
+        const bool activation_rate_on,
+        const bool maintenance_rate_on,
+        const bool shortening_rate_on,
+        const bool basal_rate_on,
+        const bool work_rate_on)
+{
+    constructProperties();
+
+    set_activation_rate_on(activation_rate_on);
+    set_maintenance_rate_on(maintenance_rate_on);
+    set_shortening_rate_on(shortening_rate_on);
+    set_basal_rate_on(basal_rate_on);
+    set_mechanical_work_rate_on(work_rate_on);
+}
+
+void SmoothBhargava2004Metabolics::constructProperties()
+{
+    constructProperty_activation_rate_on(true);
+    constructProperty_maintenance_rate_on(true);
+    constructProperty_shortening_rate_on(true);
+    constructProperty_basal_rate_on(true);
+    constructProperty_mechanical_work_rate_on(true);
+    constructProperty_enforce_minimum_heat_rate_per_muscle(true);
+
+    const int curvePoints = 5;
+    const double curveX[] = {0.0, 0.5, 1.0, 1.5, 2.0};
+    const double curveY[] = {0.5, 0.5, 1.0, 0.0, 0.0};
+    PiecewiseLinearFunction fiberLengthDepCurveDefault(curvePoints, curveX,
+            curveY, "defaultCurve");
+    constructProperty_normalized_fiber_length_dependence_on_maintenance_rate(
+            fiberLengthDepCurveDefault);
+
+    constructProperty_use_force_dependent_shortening_prop_constant(false);
+    constructProperty_basal_coefficient(1.2);
+    constructProperty_basal_exponent(1.0);
+    constructProperty_muscle_effort_scaling_factor(1.0);
+    constructProperty_include_negative_mechanical_work(true);
+    constructProperty_forbid_negative_total_power(true);
+    constructProperty_SmoothBhargava2004Metabolics_MetabolicMuscleParameterSet
+       (SmoothBhargava2004Metabolics_MetabolicMuscleParameterSet());
+}
 
 double SmoothBhargava2004Metabolics::getTotalMetabolicRate(
         const SimTK::State& s) const {
-    return getMetabolicRate(s).sum();
+
+    // BASAL METABOLIC RATE (W) (based on whole body mass, not muscle mass)
+    // ------------------------------------------------------------------
+    double Bdot = 0;
+    if (get_basal_rate_on()) {
+        Bdot = get_basal_coefficient()
+            * pow(_model->getMatterSubsystem().calcSystemMass(s),
+                    get_basal_exponent());
+        if (SimTK::isNaN(Bdot))
+            std::cout << "WARNING::" << getName();
+            std::cout << ": Bdot = NaN!" << std::endl;
+    }
+
+    return getMetabolicRate(s).sum() + Bdot;
 }
 
 double SmoothBhargava2004Metabolics::getMuscleMetabolicRate(
@@ -128,13 +188,6 @@ void SmoothBhargava2004Metabolics::extendConnectToModel(Model& model) {
     int i = 0;
     for (const auto& muscle : model.getComponentList<Muscle>()) {
         if (muscle.get_appliesForce()) {
-            // This "maxPower" is Fmax * Vmax.
-            // Must convert max contraction velocity from
-            // optimal_fiber_lengths/second to meters/second.
-            const double maxPower = muscle.get_max_isometric_force() *
-                                    muscle.get_max_contraction_velocity() *
-                                    muscle.get_optimal_fiber_length();
-            m_muscles.emplace_back(&muscle, maxPower);
             m_muscleIndices[muscle.getAbsolutePathString()] = i;
             ++i;
         }
@@ -160,22 +213,151 @@ const SimTK::Vector& SmoothBhargava2004Metabolics::getMetabolicRate(
 void SmoothBhargava2004Metabolics::calcMetabolicRate(
         const SimTK::State& s, SimTK::Vector& ratesForMuscles) const {
     ratesForMuscles.resize((int)m_muscles.size());
+
+    double Adot, Mdot, Sdot, Wdot;
+    Adot = Mdot = Sdot = Wdot = 0;
+
     int i = 0;
     for (const auto& entry : m_muscles) {
-        const double activation = entry.first->getActivation(s);
-        const double& maxPower = entry.second;
-        const double normFiberShortVel =
-                -entry.first->getNormalizedFiberVelocity(s);
 
-        const double numerator =
-                0.054 * normFiberShortVel * (0.506 + normFiberShortVel * 2.46);
-        const double denominator =
-                1 +
-                normFiberShortVel *
-                        (-1.13 + normFiberShortVel *
-                                         (12.8 + normFiberShortVel * -1.64));
+        SmoothBhargava2004Metabolics_MetabolicMuscleParameters& mm =
+            get_SmoothBhargava2004Metabolics_MetabolicMuscleParameterSet()[i];
 
-        ratesForMuscles[i] = activation * maxPower * numerator / denominator;
+        const double max_isometric_force = entry->getMaxIsometricForce();
+        const double activation = get_muscle_effort_scaling_factor()
+                                  * entry->getActivation(s);
+        const double excitation = get_muscle_effort_scaling_factor()
+                                  * entry->getControl(s);
+        const double fiber_force_passive = entry->getPassiveFiberForce(s);
+        const double fiber_force_active = get_muscle_effort_scaling_factor()
+                                          * entry->getActiveFiberForce(s);
+        const double fiber_force_total = fiber_force_active
+                                         + fiber_force_passive;
+        const double fiber_length_normalized =
+            entry->getNormalizedFiberLength(s);
+        const double fiber_velocity = entry->getFiberVelocity(s);
+
+        const double slow_twitch_excitation = mm.get_ratio_slow_twitch_fibers()
+                                              * sin(SimTK::Pi/2 * excitation);
+        const double fast_twitch_excitation =
+            (1 - mm.get_ratio_slow_twitch_fibers())
+            * (1 - cos(SimTK::Pi/2 * excitation));
+        double alpha, fiber_length_dependence;
+
+        // Get the unnormalized total active force, F_iso that 'would' be
+        // developed at the current activation and fiber length under isometric
+        // conditions (i.e. Vm=0)
+        const double F_iso = activation
+                             * entry->getActiveForceLengthMultiplier(s)
+                             * max_isometric_force;
+
+        // ACTIVATION HEAT RATE (W)
+        // ------------------------
+        if (get_forbid_negative_total_power() || get_activation_rate_on())
+        {
+            // This value is set to 1.0, as used by Anderson & Pandy (1999),
+            // however, in Bhargava et al., (2004) they assume a function here.
+            // We will ignore this function and use 1.0 for now.
+            const double decay_function_value = 1.0;
+            Adot = mm.getMuscleMass() * decay_function_value
+                * ( (mm.get_activation_constant_slow_twitch()
+                            * slow_twitch_excitation)
+                        + (mm.get_activation_constant_fast_twitch()
+                                * fast_twitch_excitation) );
+        }
+
+        // MAINTENANCE HEAT RATE (W)
+        // -------------------------
+        if (get_forbid_negative_total_power() || get_maintenance_rate_on())
+        {
+            SimTK::Vector tmp(1, fiber_length_normalized);
+            fiber_length_dependence =
+                get_normalized_fiber_length_dependence_on_maintenance_rate().
+                    calcValue(tmp);
+            Mdot = mm.getMuscleMass() * fiber_length_dependence
+                * ( (mm.get_maintenance_constant_slow_twitch()
+                        * slow_twitch_excitation)
+                    + (mm.get_maintenance_constant_fast_twitch()
+                            * fast_twitch_excitation) );
+        }
+
+        // SHORTENING HEAT RATE (W)
+        // --> note that we define Vm<0 as shortening and Vm>0 as lengthening
+        // --------------------------------------------------------------------
+        if (get_forbid_negative_total_power() || get_shortening_rate_on())
+        {
+            if (get_use_force_dependent_shortening_prop_constant())
+            {
+                if (fiber_velocity <= 0)    // concentric contraction, Vm<0
+                    alpha = (0.16 * F_iso) + (0.18 * fiber_force_total);
+                else                        // eccentric contraction, Vm>0
+                    alpha = 0.157 * fiber_force_total;
+            }
+            else
+            {
+                if (fiber_velocity <= 0)    // concentric contraction, Vm<0
+                    alpha = 0.25 * fiber_force_total;
+                else                        // eccentric contraction, Vm>0
+                    alpha = 0.0;
+            }
+            Sdot = -alpha * fiber_velocity;
+        }
+
+        // MECHANICAL WORK RATE for the contractile element of the muscle (W).
+        // --> note that we define Vm<0 as shortening and Vm>0 as lengthening.
+        // -------------------------------------------------------------------
+        if (get_forbid_negative_total_power() || get_mechanical_work_rate_on())
+        {
+            if (get_include_negative_mechanical_work() || fiber_velocity <= 0)
+                Wdot = -fiber_force_active*fiber_velocity;
+            else
+                Wdot = 0;
+        }
+
+        // If necessary, increase the shortening heat rate so that the total
+        // power is non-negative.
+        if (get_forbid_negative_total_power()) {
+            const double Edot_W_beforeClamp = Adot + Mdot + Sdot + Wdot;
+            if (Edot_W_beforeClamp < 0)
+                Sdot -= Edot_W_beforeClamp;
+        }
+
+        // This check is adapted from Umberger(2003), page 104: the total heat
+        // rate (i.e., Adot + Mdot + Sdot) for a given muscle cannot fall below
+        // 1.0 W/kg.
+        // --------------------------------------------------------------------
+        double totalHeatRate = Adot + Mdot + Sdot;
+
+        if(get_enforce_minimum_heat_rate_per_muscle()
+                    && totalHeatRate < 1.0 * mm.getMuscleMass()
+                    && get_activation_rate_on()
+                    && get_maintenance_rate_on()
+                    && get_shortening_rate_on())
+        {
+                totalHeatRate = 1.0 * mm.getMuscleMass();
+        }
+
+        // TOTAL METABOLIC ENERGY RATE (W)
+        // -------------------------------
+        double Edot = 0;
+
+        if (get_activation_rate_on() && get_maintenance_rate_on()
+            && get_shortening_rate_on())
+        {
+            Edot += totalHeatRate; // May have been clamped to 1.0 W/kg.
+        }
+        else {
+            if (get_activation_rate_on())
+                Edot += Adot;
+            if (get_maintenance_rate_on())
+                Edot += Mdot;
+            if (get_shortening_rate_on())
+                Edot += Sdot;
+        }
+        if (get_mechanical_work_rate_on())
+            Edot += Wdot;
+
+        ratesForMuscles[i] = Edot;
         ++i;
     }
 }
--- Moco/Moco/Components/SmoothBhargava2004Metabolics.h
+++ Moco/Moco/Components/SmoothBhargava2004Metabolics.h
@@ -21,6 +21,7 @@
 #include "../osimMocoDLL.h"
 #include <unordered_map>
 
+#include <OpenSim/Common/PiecewiseLinearFunction.h>
 #include <OpenSim/Simulation/Model/ModelComponent.h>
 #include <OpenSim/Simulation/Model/Muscle.h>
 
@@ -28,10 +29,10 @@ namespace OpenSim {
 
 /// Object class that holds the metabolic parameters required to calculate
 /// metabolic power for a single muscle.
-class OSIMMOCO_API SmoothBhargava2004Metabolics_MetabolicMusclePrameters :
+class OSIMMOCO_API SmoothBhargava2004Metabolics_MetabolicMuscleParameters :
         public Object {
     OpenSim_DECLARE_CONCRETE_OBJECT(
-            SmoothBhargava2004Metabolics_MetabolicMusclePrameters, Object);
+            SmoothBhargava2004Metabolics_MetabolicMuscleParameters, Object);
 public:
     OpenSim_DECLARE_PROPERTY(specific_tension, double,
         "The specific tension of the muscle (Pascals (N/m^2)).");
@@ -54,12 +55,12 @@ public:
         "Maintenance constant for slow twitch fibers (W/kg).");
     OpenSim_DECLARE_PROPERTY(maintenance_constant_fast_twitch, double,
         "Maintenance constant for fast twitch fibers (W/kg).");
-    SmoothBhargava2004Metabolics_MetabolicMusclePrameters();
-    SmoothBhargava2004Metabolics_MetabolicMusclePrameters(
+    SmoothBhargava2004Metabolics_MetabolicMuscleParameters();
+    SmoothBhargava2004Metabolics_MetabolicMuscleParameters(
             const std::string& muscleName,
             double ratio_slow_twitch_fibers,
             double muscle_mass = SimTK::NaN);
-    SmoothBhargava2004Metabolics_MetabolicMusclePrameters(
+    SmoothBhargava2004Metabolics_MetabolicMuscleParameters(
         const std::string& muscleName,
         double ratio_slow_twitch_fibers,
         double activation_constant_slow_twitch,
@@ -80,15 +81,15 @@ private:
 /// Object class that holds the set of metabolic parameters required to
 /// calculate metabolic power for a single muscle.
 class OSIMMOCO_API
-    SmoothBhargava2004Metabolics_MetabolicMusclePrameterSet
-    : public Set<SmoothBhargava2004Metabolics_MetabolicMusclePrameters>
+    SmoothBhargava2004Metabolics_MetabolicMuscleParameterSet
+    : public Set<SmoothBhargava2004Metabolics_MetabolicMuscleParameters>
 {
     OpenSim_DECLARE_CONCRETE_OBJECT(
-        SmoothBhargava2004Metabolics_MetabolicMusclePrameterSet,
-        Set<SmoothBhargava2004Metabolics_MetabolicMusclePrameters>);
+        SmoothBhargava2004Metabolics_MetabolicMuscleParameterSet,
+        Set<SmoothBhargava2004Metabolics_MetabolicMuscleParameters>);
 
 public:
-    SmoothBhargava2004Metabolics_MetabolicMusclePrameterSet() {}
+    SmoothBhargava2004Metabolics_MetabolicMuscleParameterSet() {}
 };
 
 
@@ -99,23 +100,72 @@ class OSIMMOCO_API SmoothBhargava2004Metabolics : public ModelComponent {
             SmoothBhargava2004Metabolics, ModelComponent);
 
 public:
+    OpenSim_DECLARE_PROPERTY(activation_rate_on, bool, "Specify whether "
+        "activation heat rate is to be calculated (default is true).");
+    OpenSim_DECLARE_PROPERTY(maintenance_rate_on, bool, "Specify whether "
+        "maintenance heat rate is to be calculated (default is true).");
+    OpenSim_DECLARE_PROPERTY(shortening_rate_on, bool, "Specify whether "
+        "shortening heat rate is to be calculated (default is true).");
+    OpenSim_DECLARE_PROPERTY(basal_rate_on, bool, "Specify whether basal heat "
+        "rate is to be calculated (default is true).");
+    OpenSim_DECLARE_PROPERTY(mechanical_work_rate_on, bool, "Specify whether "
+        "mechanical work rate is to be calculated (default is true).");
+    OpenSim_DECLARE_PROPERTY(enforce_minimum_heat_rate_per_muscle, bool,
+        "Specify whether the total heat rate for a muscle will be clamped to "
+        "a minimum value of 1.0 W/kg (default is true).");
+    OpenSim_DECLARE_PROPERTY(
+        normalized_fiber_length_dependence_on_maintenance_rate,
+        PiecewiseLinearFunction,
+        "Contains a PiecewiseLinearFunction object that describes the "
+        "normalized fiber length dependence on maintenance rate.");
+    OpenSim_DECLARE_PROPERTY(use_force_dependent_shortening_prop_constant,
+        bool, "Specify whether to use a force dependent shortening "
+        "proportionality constant (default is false).");
+    OpenSim_DECLARE_PROPERTY(basal_coefficient, double, "Basal metabolic "
+        "coefficient (default is 1.2).");
+    OpenSim_DECLARE_PROPERTY(basal_exponent, double, "Basal metabolic "
+        "exponent (default is 1).");
+    OpenSim_DECLARE_PROPERTY(muscle_effort_scaling_factor, double,
+        "Scale the excitation and activation values to compensate for "
+        "solutions with excessive coactivation (e.g., when a suboptimal "
+        "tracking strategy is used) (default is 1).");
+    OpenSim_DECLARE_PROPERTY(include_negative_mechanical_work, bool,
+        "Specify whether negative mechanical work will be included in Wdot "
+        "(default is true).");
+    OpenSim_DECLARE_PROPERTY(forbid_negative_total_power, bool,
+        "Specify whether the total power for each muscle must remain positive "
+        "(default is true).");
+
+    OpenSim_DECLARE_UNNAMED_PROPERTY(
+        SmoothBhargava2004Metabolics_MetabolicMuscleParameterSet,
+        "A set containing, for each muscle, the parameters "
+        "required to calculate muscle metabolic power.");
+
     OpenSim_DECLARE_OUTPUT(total_metabolic_rate, double, getTotalMetabolicRate,
             SimTK::Stage::Velocity);
     OpenSim_DECLARE_LIST_OUTPUT(muscle_metabolic_rate, double,
             getMuscleMetabolicRate, SimTK::Stage::Velocity);
+
+    SmoothBhargava2004Metabolics();
+    SmoothBhargava2004Metabolics(
+            const bool activation_rate_on,
+            const bool maintenance_rate_on,
+            const bool shortening_rate_on,
+            const bool basal_rate_on,
+            const bool work_rate_on);
 
     double getTotalMetabolicRate(const SimTK::State& s) const;
     double getMuscleMetabolicRate(
             const SimTK::State& s, const std::string& channel) const;
 
 private:
+    void constructProperties();
     void extendConnectToModel(Model&) override;
     void extendAddToSystem(SimTK::MultibodySystem&) const override;
     const SimTK::Vector& getMetabolicRate(const SimTK::State& s) const;
     void calcMetabolicRate(const SimTK::State& s, SimTK::Vector&) const;
 
-    mutable std::vector<std::pair<SimTK::ReferencePtr<const Muscle>, double>>
-            m_muscles;
+    mutable std::vector<SimTK::ReferencePtr<const Muscle>> m_muscles;
     mutable std::unordered_map<std::string, int> m_muscleIndices;
 };
 
