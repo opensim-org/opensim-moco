1bdd3f212d24dd12f7c783fd07c9c91105ab31ec
Christopher Dembia <cld72@cornell.edu>
2020-05-16 11:54:35 -0700

    Use ezc3d.

--- dependencies/opensim-core.cmake
+++ dependencies/opensim-core.cmake
@@ -4,7 +4,7 @@
 # will invalidate its cached opensim-core installation if we change the commit.
 # This commented commit hash is not actually used in the superbuild.
 # opensim-core commit:
-# 60dd70dc04d122569587be2aecfeaebf57e465ac
+# 2f5d4b09722e57359e2ff1787328527050a4d34e
 
 AddDependency(NAME       opensim-core
               URL        ${CMAKE_SOURCE_DIR}/../opensim-core
@@ -15,17 +15,18 @@ AddDependency(NAME       opensim-core
                     -DBUILD_PYTHON_WRAPPING:BOOL=${OPENSIM_PYTHON_WRAPPING}
                     -DOPENSIM_PYTHON_VERSION:STRING=${OPENSIM_PYTHON_VERSION}
                     -DOPENSIM_DEPENDENCIES_DIR:PATH=${CMAKE_INSTALL_PREFIX}
-                    -DWITH_BTK:BOOL=ON
+                    -DOPENSIM_C3D_PARSER:STRING=ezc3d
                     -DOPENSIM_INSTALL_UNIX_FHS:BOOL=${OPENSIM_INSTALL_UNIX_FHS})
 
 
 if(SUPERBUILD_opensim-core)
 
     # OpenSim's dependencies.
-    AddDependency(NAME       BTK
-                  GIT_URL    https://github.com/opensim-org/BTKCore.git
-                  GIT_TAG    6d787d0be223851a8f454f2ee8c7d9e47b84cbbe
-                  CMAKE_ARGS -DBUILD_SHARED_LIBS:BOOL=ON)
+    AddDependency(NAME       ezc3d
+                  DEFAULT    OFF
+                  GIT_URL        https://github.com/pyomeca/ezc3d.git
+                  GIT_TAG        Release_1.3.2
+                  CMAKE_ARGS -DBUILD_EXAMPLE:BOOL=OFF)
 
     AddDependency(NAME simbody
                   GIT_URL    https://github.com/simbody/simbody.git
@@ -38,7 +39,7 @@ if(SUPERBUILD_opensim-core)
                   GIT_TAG    3dd23e3280f213bacefdf5fcb04857bf52e90917
                   CMAKE_ARGS -DCMAKE_DEBUG_POSTFIX:STRING=_d)
 
-    add_dependencies(opensim-core BTK simbody docopt)
+    add_dependencies(opensim-core ezc3d simbody docopt)
 endif()
 
 
