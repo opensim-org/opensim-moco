79a8506218fde997130465eb640803ac17675b3d
Nicholas Bianco <nbianco@stanford.edu>
2020-06-15 11:07:38 -0700

    Fix muscle passive curve integral

--- Moco/Sandbox/DeGrooteFregly2016MuscleDerivatives.m
+++ Moco/Sandbox/DeGrooteFregly2016MuscleDerivatives.m
@@ -6,6 +6,12 @@ fprintf('======================================= \n')
 f_t = c1*exp(kT*(lTtilde - c2)) - c3;
 simplify(diff(f_t, lTtilde))
 
+fprintf('Integral of tendon force length curve \n')
+fprintf('===================================== \n')
+
+f_t = c1*exp(kT*(lTtilde - c2)) - c3;
+simplify(int(f_t, lTtilde))
+
 fprintf('Derivative of active fiber force length curve \n')
 fprintf('============================================= \n')
 
@@ -22,18 +28,23 @@ simplify(diff(f_v, vMtilde))
 fprintf('Derivative of passive fiber force length curve \n')
 fprintf('============================================== \n')
 
-f_pas = (exp(kPE * (lMtilde - 1.0) / e0) - exp(kPE * (min_lMtilde - 1) / e0)) / ...
-        (exp(kPE) - exp(kPE * (min_lMtilde - 1) / e0));
+offset = exp(kPE * (min_lMtilde - 1.0) / e0);
+denom = exp(kPE) - offset;
+f_pas = (exp(kPE * (lMtilde - 1.0) / e0) - offset) / denom;
 simplify(diff(f_pas, lMtilde))
-
-fprintf('Integral of tendon force length curve \n')
-fprintf('===================================== \n')
-
-f_t = c1*exp(kT*(lTtilde - c2)) - c3;
-simplify(int(f_t, lTtilde))
 
 fprintf('Integral of passive fiber force length curve \n')
 fprintf('============================================ \n')
 
-f_pas_int = simplify(int(f_pas, lMtilde))
+f_pas_int = simplify(int(f_pas, lMtilde));
 pretty(f_pas_int)
+
+temp1 = exp(kPE * lMtilde / e0);
+temp2 = exp(kPE * min_lMtilde / e0);
+temp3 = exp(kPE * (e0 + 1.0) / e0);
+numer = - e0 * temp1 + kPE * lMtilde * temp2;
+denom = kPE * (temp2 - temp3);
+f_pas_int_decomp = numer / denom;
+
+fprintf('Expressions equal: ')
+isequal(f_pas_int, f_pas_int_decomp)
